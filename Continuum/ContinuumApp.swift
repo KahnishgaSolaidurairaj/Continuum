d4571bcde5612c90e1e1efd1fa22ed6100f7890b
KahnishgaSolaidurairaj <krishnakani19@gmail.com>
2026-06-30 14:55:10 -0500

    Initial Commit

---
+++ Continuum/ContinuumApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContinuumApp.swift
+//  Continuum
+//
+//  Created by Kahnishga Solaidurairaj on 6/30/26.
+//
+
+import SwiftUI
+
+@main
+struct ContinuumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
